2991b9c653ced716d4d8416bbfba5fbb53aab840
AbelSM <sexto_abel@hotmai.com>
2023-02-06 23:34:35 +0100

    Tarea 4 tema 6 empezada

---
+++ Tema-6/A4-U6-AbelSM/A04-U6-abel.js
@@ -0,0 +1,15 @@
+var calificaciones = [];
+
+function crearAlumno() {
+    if (calificaciones.length < 5) {
+        let alumno = document.getElementById("nombre").value;
+        let nota1 = document.getElementById("nombre").value;
+        let nota2 = document.getElementById("nombre").value;
+        let nota3 = document.getElementById("nombre").value;
+        if (!alumno && !nota1 && !nota2 && nota3) {
+            alert("No están")
+            
+        }
+    }
+
+}
