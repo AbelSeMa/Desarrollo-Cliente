70584b31c3dd8def3138e42b72df729ec5f99f17
AbelSM <sexto_abel@hotmai.com>
2023-02-07 02:43:54 +0100

    Ejercicio 5 terminado

---
+++ Tema-6/A5-U6-AbelSM/A5-U6-AbelSM.js
@@ -0,0 +1,14 @@
+document.getElementById("quitarQuijote").addEventListener('click', () => {
+    var quijote =  document.getElementById("quijote");
+    quijote.classList.toggle('ocultar')
+})
+
+document.getElementById("quitarTarzan").addEventListener('click', () => {
+    let tarzan =  document.getElementById("tarzan");
+    tarzan.classList.toggle('ocultar')
+})
+
+document.getElementById("quitarMowgli").addEventListener('click', () => {
+    var mowgli =  document.getElementById("mowgli");
+    mowgli.classList.toggle('ocultar')
+})
