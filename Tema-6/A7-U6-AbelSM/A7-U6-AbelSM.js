02cedea814825342d26bbdd23921a4b2389f5b58
AbelSM <sexto_abel@hotmai.com>
2023-02-07 17:26:10 +0100

    Primer intento de pintar el DOM conseguido

---
+++ Tema-6/A7-U6-AbelSM/A7-U6-AbelSM.js
@@ -0,0 +1,24 @@
+var todo = document.body.childNodes;
+
+todo.forEach(nodo => {
+    if (nodo.nodeType != 3 && nodo.hasChildNodes) {
+        document.body.innerHTML += nodo.nodeName + "<br>"
+        if (nodo.hasChildNodes) {
+            var hijos = nodo.childNodes;
+            hijos.forEach(hijo => {
+                if (hijo.nodeType != 3) {
+                    document.body.innerHTML += "<ul>" + hijo.nodeName + "</ul>";
+                    
+                }
+
+                if (hijo.hasChildNodes) {
+                    var nietos = hijo.childNodes;
+                    // document.body.innerHTML += nietos[0].parentNode.nodeName + "<br>";
+                    nietos.forEach(nieto => {
+                        if (nieto.nodeType != 3) {
+                            document.body.innerHTML += "<ul><ul>" + nieto.nodeName + "</ul></ul>";
+                        }
+                    });
+                }
+            })
+        }}});
